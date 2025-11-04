efeeba02652650f66a485cf0c02544d91f6a11b7
salehin-kazi <salehinkazi@outlook.com>
2025-11-03 22:58:41 -0700

    Revert "Add jenga-frontend"
    This reverts commit 828e579021d7ea4ee17c2974201be760ee5803d5.

--- src/components/ui/Textarea.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
-
-export default function Textarea(props: TextareaProps) {
-  return <textarea className={`textarea ${props.className ?? ''}`} {...props} />;
-}
-
