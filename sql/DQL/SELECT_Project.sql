a20bead1ef465e5a400d930117ea2190fb14e630
Mark Angelo Racca <43604363+markangeloracca@users.noreply.github.com>
2025-11-25 12:38:49 -0700

    Update SELECT statements

--- sql/DQL/SELECT_Project.sql
+++ sql/DQL/SELECT_Project.sql
@@ -1,1 +1,3 @@
 SELECT * FROM jengadb.Project;
+
+SELECT p.name, u. FROM jengadb.Project p LEFT JOIN jengadb.User u on p.userId = u.id;
--- sql/DQL/SELECT_User.sql
+++ sql/DQL/SELECT_User.sql
@@ -1,1 +1,11 @@
+USE jengadb;
+
 SELECT * FROM jengadb.User;
+
+SELECT CONCAT(u.firstName, " ", u.lastName) as fullName,
+       email
+FROM jengadb.User u
+WHERE u.status = "active" AND u.role = "regular";
+
+SELECT COUNT(*) FROM jengadb.User u
+WHERE u.status = "active" AND u.role = "regular";
